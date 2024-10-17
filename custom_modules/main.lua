25978391cd6568278d91361f05c4950d8d046c5e
Wu Kui <wukui@xinghuihuyu.cn>
2024-10-17 07:58:44 +0000

    add custom module example

---
+++ custom_modules/main.lua
@@ -0,0 +1,4 @@
+local mod = require("mymath")  --从文件中导入模块
+
+print(mod.add(1, 2))
+print(mod.power(2, 3))
---
+++ custom_modules/mymath.lua
@@ -0,0 +1,11 @@
+_G.mmath = {}
+
+function mmath.add(x, y)
+    return x + y
+end
+
+function mmath.power(n1, n2)
+    return n1 ^  n2
+end
+
+return mmath
